bd12740511ae1af840df376eee6936c06897709c
Harry Chen <i@harrychen.xyz>
2020-12-04 22:28:04 +0800

    Add script to run on my server for spmv
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- spmv/run.sh
+++ spmv/run.sh
@@ -1,15 +1,5 @@
-# !/bin/bash
+#!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <executable>" >&2
-  exit 1
-fi
+source ./spmv_common.sh $*
 
-export DAPL_DBG_TYPE=0
-
-DATAPATH=/home/course/HW/spmv_data
-
-EXECUTABLE=$1
-REP=64
-
-srun -p gpu ${EXECUTABLE} ${REP} ${DATAPATH}/parabolic_fem.csr
+${EXEC_PREFIX} ${EXECUTABLE} ${REP} ${DATAPATH}/parabolic_fem.csr
---
+++ spmv/spmv_common.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <executable>" >&2
+    exit 1
+fi
+
+export DAPL_DBG_TYPE=0
+
+if [ -d "/mnt/ssd/harry/spmv_data" ]; then
+  DATAPATH="/mnt/ssd/harry/spmv_data"
+else
+  DATAPATH="/home/course/HW/spmv_data"
+fi
+
+SRUN=$(which SRUN)
+
+if [ -x "$SRUN" ]; then
+    EXEC_PREFIX="$SRUN -N 1 --partition=gpu --exclusive --pty"
+fi
+
+export EXEC_PREFIX
+export DATAPATH
+export EXECUTABLE=$1
+export REP=64
