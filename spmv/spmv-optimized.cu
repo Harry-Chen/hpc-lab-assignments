902cb2721c7284f4215e89037587d33310baf4c3
Harry Chen <i@harrychen.xyz>
2020-12-14 16:56:38 +0800

    Use shfl_sync
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- spmv/spmv-optimized.cu
+++ spmv/spmv-optimized.cu
@@ -17,7 +17,11 @@
 #endif
 
 #ifndef ENABLE_SORT_BASED
-#define ENABLE_SORT_BASED 1
+#define ENABLE_SORT_BASED 0
+#endif
+
+#ifndef NTASKS_PER_THREAD
+#define NTASKS_PER_THREAD 4
 #endif
 
 const char* version_name = "optimized version";
@@ -93,6 +97,7 @@ void destroy_additional_info(void *additional_info) {
 }
 
 
+// merge-based spmv (assign some task to each thread)
 __global__ void spmv_merge_based_kernel(int m, int nnz, int ntasks_per_thread, 
     const index_t *__restrict__ r_pos, const index_t *__restrict__ c_idx, const data_t *__restrict__ values,
     const data_t *__restrict__ x, data_t *__restrict__ y) {
@@ -119,7 +124,7 @@ __global__ void spmv_merge_based_kernel(int m, int nnz, int ntasks_per_thread,
     int ntasks = MIN(ntasks_per_thread, m + nnz - curr_row - curr_index); // task number for each thread
     bool self_row = curr_index == r_pos[curr_row];
 
-    data_t res = 0.0;
+    data_t res = 0;
  
     if (curr_row < m && curr_index < nnz) {
 #pragma unroll
@@ -133,36 +138,90 @@ __global__ void spmv_merge_based_kernel(int m, int nnz, int ntasks_per_thread,
                 }
                 curr_row++;
                 self_row = true;
-                res = 0.0;
+                res = 0;
             } else {
                 res += x[c_idx[curr_index]] * values[curr_index];
                 curr_index++;
             }
         }
-        if (curr_row < m) ATOMIC_ADD_DOUBLE(&y[curr_row], res);
+        // save unfinished work
+        if (curr_row < m) {
+            // found peers with same row number
+            auto peers = get_peers(curr_row);
+            // reduce to first lane
+            res = reduce_peers(peers, res);
+            // store on only first lane
+            if ((threadIdx.x & 31) == __ffs(peers) - 1) {
+                ATOMIC_ADD_DOUBLE(&y[curr_row], res);
+            }
+        }
     }
 }
 
+
+// assign each row sorted by nnz to each thread
 __global__ void spmv_sort_based_kernel(int m, int nnz, const data_t *__restrict__ values,
     const data_t *__restrict__ x, data_t *__restrict__ y, csr_info_t *__restrict__ info) {
 
     int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-    int warp_offset = info->sorted_offset[i / 32] + i % 32;
+    int warp_offset = info->sorted_offset[i >> 5] + (threadIdx.x & 31);
     int row_index = info->row_index[i];
     int row_task_num = info->row_task_num[i];
     int thread_offset = warp_offset;
 
     if (i < m && row_task_num > 0) {
-        double sum = 0.0;
+        double sum = y[row_index];
         for (int j = 0; j < row_task_num; ++j) {
             sum += info->values_sorted[thread_offset] * x[info->c_idx_sorted[thread_offset]];
             thread_offset += 32;
         }
         y[row_index] = sum;
     }
+}
+
+
+// assign each row sequentially to each warp
+__global__ void smpv_warp_based_kernel(int m, const index_t *__restrict__ r_pos, const index_t *__restrict__ c_idx, const data_t *__restrict__ values, const data_t *__restrict__ x, data_t *__restrict__ y) {
+    
+    int lane_id = threadIdx.x & 31;
+    int warp_row = (blockIdx.x * blockDim.x + threadIdx.x) >> 5;
+
+    if (warp_row < m) {
+        int begin = r_pos[warp_row];
+        int end = r_pos[warp_row + 1];
+        data_t res = y[warp_row];
+        // each lane accumulates data
+        for (int i = lane_id + begin; i < end; i += 32) {
+            res += values[i] * x[c_idx[i]];
+        }
+        // accumulates to lane 0
+        for (int i = 16; i > 0; i >>= 1) {
+            res += __shfl_down_sync(0xffffffff, res, i);
+        }
+        if (lane_id == 0) {
+            y[warp_row] = res;
+        }
+    }
+}
+
+
+// assign each row sequentially to each thread
+__global__ void spmv_naive_kernel(int m, const index_t *__restrict__ r_pos, const index_t *__restrict__ c_idx, const data_t *__restrict__ values, const data_t *__restrict__ x, data_t *__restrict__ y) {
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < m) {
+        int begin = r_pos[i], end = r_pos[i+1];
+        data_t s = y[i];
+        for (int p = begin; p < end; ++p) {
+            int j = c_idx[p];
+            s += values[p] * x[j];
+        }
+        y[i] = s;
+    }
 
 }
+
 
 void spmv(dist_matrix_t *mat, const data_t *__restrict__ x, data_t *__restrict__ y) {
 
@@ -170,14 +229,15 @@ void spmv(dist_matrix_t *mat, const data_t *__restrict__ x, data_t *__restrict__
     int n = mat->global_nnz;
 
 #if ENABLE_SORT_BASED
-    dim3 grid_size(ceiling(m, 1024), 1, 1);
-    dim3 block_size(1024, 1, 1);
+    dim3 grid_size(ceiling(m, BLOCK_SIZE), 1, 1);
+    dim3 block_size(BLOCK_SIZE, 1, 1);
     spmv_sort_based_kernel<<<grid_size, block_size>>>(m, n, mat->gpu_values, x, y, (csr_info_t *)mat->additional_info);
+    // smpv_warp_based_kernel<<<grid_size, block_size>>>(m, mat->gpu_r_pos, mat->gpu_c_idx, mat->gpu_values, x, y);
     CUDA_CHECK(cudaGetLastError());
 #else
-    dim3 grid_size(GRID_SIZE, 1, 1);
+    int ntasks_per_thread = NTASKS_PER_THREAD;
+    dim3 grid_size(ceiling(m + n, BLOCK_SIZE * ntasks_per_thread), 1, 1);
     dim3 block_size(BLOCK_SIZE, 1, 1);
-    int ntasks_per_thread = ceiling(m + n, GRID_SIZE * BLOCK_SIZE);
     spmv_merge_based_kernel<<<grid_size, block_size>>>(m, n, ntasks_per_thread, mat->gpu_r_pos, mat->gpu_c_idx, mat->gpu_values, x, y);
     CUDA_CHECK(cudaGetLastError());
 #endif
--- spmv/utils.h
+++ spmv/utils.h
@@ -42,6 +42,8 @@ int check_answer(dist_matrix_t *mat, const char* filename, data_t* y);
 
 #endif
 
+#ifdef __NVCC__
+
 #if __CUDA_ARCH__ < 600
 #define ATOMIC_ADD_DOUBLE atomicAddDouble
 __device__ inline double atomicAddDouble(double* address, double val) {
@@ -62,4 +64,69 @@ __device__ inline double atomicAddDouble(double* address, double val) {
 }
 #else
 #define ATOMIC_ADD_DOUBLE atomicAdd
+#endif
+
+template<typename G>
+__device__ __inline__ uint get_peers(G key) {
+    uint peers=0;
+    bool is_peer;
+
+    // in the beginning, all lanes are available
+    uint unclaimed=0xffffffff;
+
+    do {
+        // fetch key of first unclaimed lane and compare with this key
+        is_peer = (key == __shfl(key, __ffs(unclaimed) - 1));
+
+        // determine which lanes had a match
+        peers = __ballot(is_peer);
+
+        // remove lanes with matching keys from the pool
+        unclaimed ^= peers;
+
+        // quit if we had a match
+    } while (!is_peer);
+
+    return peers;
+}
+
+template <typename F>
+__device__ __inline__ F reduce_peers(uint peers, F &x) {
+    int lane = threadIdx.x & 31;
+
+    // find the peer with lowest lane index
+    int first = __ffs(peers)-1;
+
+    // calculate own relative position among peers
+    int rel_pos = __popc(peers << (32 - lane));
+
+    // ignore peers with lower (or same) lane index
+    peers &= (0xfffffffe << lane);
+
+    while(__any(peers)) {
+        // find next-highest remaining peer
+        int next = __ffs(peers);
+
+        // __shfl() only works if both threads participate, so we always do.
+        F t = __shfl(x, next - 1);
+
+        // only add if there was anything to add
+        if (next) x += t;
+
+        // all lanes with their least significant index bit set are done
+        uint done = rel_pos & 1;
+
+        // remove all peers that are already done
+        peers &= ~__ballot(done);
+
+        // abuse relative position as iteration counter
+        rel_pos >>= 1;
+    }
+
+    // distribute final result to all peers (optional)
+    F res = __shfl(x, first);
+
+    return res;
+}
+
 #endif
